8e40e0abee9195a1ed03fd682e7ee6c860939846
Kethableez <mocarto98@gmail.com>
2022-03-15 23:38:11 +0100

    Added base request path for group controller

--- api/source/resources/group/groupController.ts
+++ api/source/resources/group/groupController.ts
@@ -16,7 +16,11 @@ class GroupController implements Controller {
   private initRoutes(): void {
     this.router.post(`${this.path}/create`, authMiddleware, this.createGroup);
     this.router.post(`${this.path}/edit/:groupId`, authMiddleware, this.editGroup);
-    this.router.post(`${this.path}/remove/:groupId`, authMiddleware, this.removeGroup);
+    this.router.post(`${this.path}/delete/:groupId`, authMiddleware, this.removeGroup);
+    this.router.post(`${this.path}/add/:groupId`, authMiddleware, this.addToGroup);
+    this.router.post(`${this.path}/remove/:groupId`, authMiddleware, this.removeFromGroup);
+    this.router.post(`${this.path}/join/:groupId`, authMiddleware, this.joinToGroup);
+    this.router.post(`${this.path}/leave/:groupId`, authMiddleware, this.leaveGroup);
   }
 
   private createGroup = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
@@ -50,6 +54,58 @@ class GroupController implements Controller {
       const userId = res.locals.user._id;
 
       const message = await this.groupService.removeGroup(userId, groupId);
+      res.status(200).json(message);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private addToGroup = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+      const founderId = res.locals.user._id;
+      const groupId = req.params.groupId;
+      const { memberEmail } = req.body;
+
+      console.log(req.body);
+
+      const message = await this.groupService.addToGroup(founderId, groupId, memberEmail);
+      res.status(200).json(message);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private removeFromGroup = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+      const founderId = res.locals.user._id;
+      const groupId = req.params.groupId;
+      const { memberId } = req.body;
+
+      const message = await this.groupService.removeFromGroup(founderId, groupId, memberId);
+      res.status(200).json(message);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private joinToGroup = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+      const memberId = res.locals.user._id;
+      const groupId = req.params.groupId;
+      const { invitationCode } = req.body;
+
+      const message = await this.groupService.joinToGroup(memberId, groupId, invitationCode);
+      res.status(200).json(message);
+    } catch (error: any) {
+      next(new HttpException(400, error.message));
+    }
+  };
+
+  private leaveGroup = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+      const memberId = res.locals.user._id.toString();
+      const groupId = req.params.groupId;
+      const message = await this.groupService.leaveGroup(memberId, groupId);
       res.status(200).json(message);
     } catch (error: any) {
       next(new HttpException(400, error.message));
--- api/source/resources/group/groupService.ts
+++ api/source/resources/group/groupService.ts
@@ -1,6 +1,9 @@
+import { string } from 'joi';
+import { includes } from 'lodash';
 import mongoose from 'mongoose';
 import { generateInvitationCode } from '../../functions/generators';
 import BaseResponse from '../../utils/baseResponseModel';
+import userSchema from '../user/userSchema';
 import groupSchema from './groupSchema';
 import CreateGroupPayload from './payload/createPayload';
 import EditGroupPayload from './payload/editGroupPayload';
@@ -9,6 +12,7 @@ const ObjectId = mongoose.Types.ObjectId;
 
 class GroupService {
   private groupSchema = groupSchema;
+  private userSchema = userSchema;
 
   public async createGroup(founderId: string, payload: CreateGroupPayload): Promise<BaseResponse | Error> {
     try {
@@ -57,6 +61,115 @@ class GroupService {
       await this.groupSchema.findByIdAndDelete(groupId);
 
       return { message: 'Remmoved' };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error('Unexpected error');
+    }
+  }
+
+  public async addToGroup(founderId: string, groupId: string, memberEmail: string): Promise<BaseResponse | Error> {
+    try {
+      const group = await this.groupSchema.findById(groupId);
+      const userToAdd = await this.userSchema.findOne({ email: memberEmail });
+
+      if (!group) throw new Error('Invalid group');
+      if (!userToAdd) throw new Error('No username with given email');
+      if (group.founder.toString() !== founderId.toString()) throw new Error('Insufficient priviliges');
+      if (userToAdd._id.toString() === founderId.toString()) throw new Error('Cannot add self to the group members');
+      if (
+        includes(
+          group.members.map((m) => m.toString()),
+          userToAdd._id.toString()
+        )
+      ) {
+        throw new Error('This user already a member');
+      }
+
+      group.members.push(new ObjectId(userToAdd._id));
+      await group.save();
+      return { message: 'User added' };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error('Unexpected error');
+    }
+  }
+
+  public async removeFromGroup(founderId: string, groupId: string, memberId: string): Promise<BaseResponse | Error> {
+    try {
+      const group = await this.groupSchema.findById(groupId);
+
+      if (!group) throw new Error('Invalid group');
+      if (group.founder.toString() !== founderId.toString()) throw new Error('Insufficient priviliges');
+      if (
+        !includes(
+          group.members.map((m) => m.toString()),
+          memberId
+        )
+      ) {
+        throw new Error('This user not exists in the group');
+      }
+
+      group.members = group.members.filter((m) => m.toString() !== memberId);
+      await group.save();
+      return { message: 'User removed' };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error('Unexpected error');
+    }
+  }
+
+  public async joinToGroup(memberId: string, groupId: string, invitationCode: string): Promise<BaseResponse | Error> {
+    try {
+      const group = await this.groupSchema.findById(groupId);
+      const userToAdd = await this.userSchema.findById(memberId);
+      if (!group) throw new Error('Invalid group');
+      if (!userToAdd) throw new Error('No username with given email');
+      if (userToAdd._id.toString() === group.founder.toString())
+        throw new Error('Cannot add self to the group members');
+      if (
+        includes(
+          group.members.map((m) => m.toString()),
+          userToAdd._id.toString()
+        )
+      ) {
+        throw new Error('This user already a member');
+      }
+      if (group.invitationCode !== invitationCode) throw new Error('Invalid invitation code');
+
+      group.members.push(new ObjectId(userToAdd._id));
+      await group.save();
+      return { message: 'User added' };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+      throw new Error('Unexpected error');
+    }
+  }
+
+  public async leaveGroup(memberId: string, groupId: string): Promise<BaseResponse | Error> {
+    try {
+      const group = await this.groupSchema.findById(groupId);
+
+      if (!group) throw new Error('Invalid group');
+      if (memberId === group.founder.toString()) throw new Error('Cannot leave your own group');
+      if (
+        !includes(
+          group.members.map((m) => m.toString()),
+          memberId
+        )
+      ) {
+        throw new Error('This user not exists in the group');
+      }
+      group.members = group.members.filter((m) => m.toString() !== memberId);
+      await group.save();
+      return { message: 'User added' };
     } catch (error: unknown) {
       if (error instanceof Error) {
         throw new Error(error.message);
