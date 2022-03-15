eb9987c06106a07c6a96254117ec6bea0e8be35a
Kethableez <mocarto98@gmail.com>
2022-03-15 14:51:32 +0100

    Forgot to include some files

---
+++ api/source/resources/user/payload/editPayload.ts
@@ -0,0 +1,11 @@
+interface EditPayload {
+  oldPassword: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  birthdate: string;
+  avatar: string;
+  isSubscribed: boolean;
+}
+
+export default EditPayload;
