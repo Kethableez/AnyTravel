7994dd26d9257698712fed241f0bfc852ce504e0
kajetan <kajetanzdanowicz@wp.pl>
2022-03-21 00:17:55 +0100

    backend config fixes

--- api/source/index.ts
+++ api/source/index.ts
@@ -3,10 +3,11 @@ import FileController from './resources/file/fileController';
 import GroupController from './resources/group/groupController';
 import UserController from './resources/user/userController';
 import Server from './server';
+import config from './config/config'
 
 const server = new Server(
   [new UserController(), new GroupController(), new FileController(), new AttractionController()],
-  Number(process.env.SERVER_PORT)
+  Number(config.server.port)
 );
 
 server.listen();
