bcd67f9bca5ec7efb27016807a753a829fdb1ea4
Kethableez <mocarto98@gmail.com>
2022-04-02 12:47:40 +0200

    Some refactor with label pipe to display values

---
+++ app/src/app/shared/pipes/label.pipe.ts
@@ -0,0 +1,98 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'label'
+})
+export class LabelPipe implements PipeTransform {
+  transform(label: any): string {
+    switch (label) {
+      // Sorting
+      case 'NAME_ASC':
+        return 'Nazwa rosnąco';
+
+      case 'NAME_DESC':
+        return 'Nazwa malejąco';
+
+      case 'RATING_ASC':
+        return 'Ocena rosnąco';
+
+      case 'RATING_DESC':
+        return 'Ocena malejąco';
+      // Filter key
+      case 'country':
+        return 'Kraj';
+
+      case 'city':
+        return 'Miasto';
+
+      case 'attractionType':
+        return 'Typ';
+
+      case 'catetory':
+        return 'Kategoria';
+
+      case 'ticketPrice':
+        return 'Cena biletów';
+
+      //Category
+      case 'RESTAURANT':
+        return 'Restauracja';
+
+      case 'MUSEUM':
+        return 'Muzeum';
+
+      case 'LIBRARY':
+        return 'Biblioteka';
+
+      case 'PARK':
+        return 'Park';
+
+      case 'BEACH':
+        return 'Plaża';
+
+      case 'LAKE':
+        return 'Jezioro';
+
+      case 'FOREST':
+        return 'Las';
+
+      case 'ART':
+        return 'Sztuka';
+
+      case 'GYM':
+        return 'Siłownia';
+
+      case 'COFFEE':
+        return 'Kawiarnia';
+
+      case 'BAR':
+        return 'Bar';
+
+      //Type
+      case 'INDOOR':
+        return 'Wewnątrz';
+
+      case 'OUTDOOR':
+        return 'Na zewnątrz';
+
+      //TicketPrice
+      case 'Free':
+        return 'Bezpłatne';
+
+      case '$':
+        return 'Tanie';
+
+      case '$$':
+        return 'Umiarkowane';
+
+      case '$$$':
+        return 'Drogie';
+
+      case '$$$$':
+        return 'Bardzo drogie';
+
+      default:
+        return label;
+    }
+  }
+}
