bae5b4c1011eab436b8abc0f83f120d3d45bb515
kajetan <kajetanzdanowicz@wp.pl>
2022-05-14 23:18:05 +0200

    cover for journeys in dashboard, front journey model

--- app/src/app/modules/main-page/pages/dashboard/dashboard.component.ts
+++ app/src/app/modules/main-page/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { RootState } from '@store/app.states';
 import { selectFutureUserJourneys, selectPastUserJourneys, selectUpcomingUserJourneys } from '@store/journey/selectors/journey.selectors'
+import { Journey } from '@models/journey/journey.model';
 
 
 @Component({
@@ -12,9 +13,16 @@ import { selectFutureUserJourneys, selectPastUserJourneys, selectUpcomingUserJou
 export class DashboardComponent implements OnInit {
   constructor(private store$: Store<RootState>) {}
   
+  @Input()
+  journey?: Journey;
+  
   userJourneys$ = this.store$.select(selectUpcomingUserJourneys);
   futureJourneys$ = this.store$.select(selectFutureUserJourneys);
   pastJourneys$ = this.store$.select(selectPastUserJourneys);
+
+  get journeyCoverFromAttraction(): string {
+    return ['http://localhost:9000/api/file/download', this.journey?.cover].join('/');
+  }
 
   ngOnInit(): void {}
 
