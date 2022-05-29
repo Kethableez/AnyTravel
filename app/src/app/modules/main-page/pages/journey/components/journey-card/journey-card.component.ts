4767568407d977726fd588bf3cee9450b8ae72c3
Kethableez <mocarto98@gmail.com>
2022-05-30 00:23:14 +0200

    Fixes

--- app/src/app/modules/main-page/pages/journey/components/journey-card/journey-card.component.ts
+++ app/src/app/modules/main-page/pages/journey/components/journey-card/journey-card.component.ts
@@ -3,13 +3,14 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { RootState } from '@store/app.states';
 import { Observable, of, tap, timer } from 'rxjs';
+import { CleanableDirective } from 'src/app/shared/directives/cleanable.directive';
 
 @Component({
   selector: 'majk-journey-card',
   templateUrl: './journey-card.component.html',
   styleUrls: ['./journey-card.component.scss']
 })
-export class JourneyCardComponent implements OnInit {
+export class JourneyCardComponent extends CleanableDirective implements OnInit {
   @Input()
   journey: any = {};
 
@@ -18,21 +19,26 @@ export class JourneyCardComponent implements OnInit {
 
   index = 0;
 
-  constructor(private store$: Store<RootState>, private router: Router) {}
+  constructor(private store$: Store<RootState>, private router: Router) {
+    super();
+  }
 
   ngOnInit(): void {
     if (this.journey) {
       this.images = this.journey.attractions.map((attraction: any) => this.getAttractionCover(attraction.cover));
     }
 
-    timer(0, 5000).subscribe((val) => {
-      this.image = this.images[this.index];
-      if (this.index === this.journey.attractions.length - 1) {
-        this.index = 0;
-      } else {
-        this.index++;
-      }
-    });
+    this.addSubscription(
+      timer(0, 5000).subscribe((val) => {
+        console.log(val);
+        this.image = this.images[this.index];
+        if (this.index === this.journey.attractions.length - 1) {
+          this.index = 0;
+        } else {
+          this.index++;
+        }
+      })
+    );
   }
 
   navigate(id: string) {
