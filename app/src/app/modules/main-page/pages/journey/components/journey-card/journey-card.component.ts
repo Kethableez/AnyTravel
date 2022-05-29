abb8c47683d3bdb2007e5d5814def8a413c90afa
Kethableez <mocarto98@gmail.com>
2022-05-29 11:19:56 +0200

    added carrousel

--- app/src/app/modules/main-page/pages/journey/components/journey-card/journey-card.component.ts
+++ app/src/app/modules/main-page/pages/journey/components/journey-card/journey-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { RootState } from '@store/app.states';
+import { Observable, of, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'majk-journey-card',
@@ -12,21 +13,33 @@ export class JourneyCardComponent implements OnInit {
   @Input()
   journey: any = {};
 
+  images: string[] = [];
+  image = '';
+
+  index = 0;
+
   constructor(private store$: Store<RootState>, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.journey) {
+      this.images = this.journey.attractions.map((attraction: any) => this.getAttractionCover(attraction.cover));
+    }
+
+    timer(0, 5000).subscribe((val) => {
+      this.image = this.images[this.index];
+      if (this.index === this.journey.attractions.length - 1) {
+        this.index = 0;
+      } else {
+        this.index++;
+      }
+    });
+  }
 
   navigate(id: string) {
     this.router.navigateByUrl(`/home/journey/${id}`);
   }
 
-  // mapAttractions(attractions: any[]) {
-  //   const dates = [...new Set(attractions.map((attraction: any) => attraction.date.split('T')[0]))] as string[];
-  //   return dates.map((date: string) => {
-  //     return {
-  //       date,
-  //       attractions: attractions.filter((attraction: any) => attraction.date.split('T')[0] === date)
-  //     };
-  //   });
-  // }
+  getAttractionCover(coverRef: string): string {
+    return coverRef.startsWith('attraction/') ? `http://localhost:9000/api/file/download/${coverRef}` : coverRef;
+  }
 }
